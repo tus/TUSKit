4b228983a0970b0656ccf910edf33289fa0528d7
Felix Geisendörfer <felix@debuggable.com>
2013-05-01 11:17:52 +0200

    Inline NSStream+BoundPair

--- TUSKit/TUSKit/TUSData.m
+++ TUSKit/TUSKit/TUSData.m
@@ -7,8 +7,6 @@
 //
 
 #import "TUSKit.h"
-#import "NSStream+BoundPair.h"
-
 #import "TUSData.h"
 
 #define TUS_BUFSIZE (32*1024)
@@ -28,7 +26,7 @@
     if (self) {
         NSInputStream* inStream = nil;
         NSOutputStream* outStream = nil;
-        [NSStream createBoundInputStream:&inStream
+        [self createBoundInputStream:&inStream
                             outputStream:&outStream
                               bufferSize:TUS_BUFSIZE];
         assert(inStream != nil);
@@ -152,6 +150,52 @@
             break;
     }
 }
+
+// A category on NSStream that provides a nice, Objective-C friendly way to create
+// bound pairs of streams.  Adapted from the SimpleURLConnections sample code.
+- (void)createBoundInputStream:(NSInputStream **)inputStreamPtr
+                  outputStream:(NSOutputStream **)outputStreamPtr
+                    bufferSize:(NSUInteger)bufferSize
+{
+    CFReadStreamRef     readStream;
+    CFWriteStreamRef    writeStream;
+
+    assert( (inputStreamPtr != NULL) || (outputStreamPtr != NULL) );
+
+    readStream = NULL;
+    writeStream = NULL;
+
+#if defined(__MAC_OS_X_VERSION_MIN_REQUIRED) && (__MAC_OS_X_VERSION_MIN_REQUIRED < 1070)
+#error If you support Mac OS X prior to 10.7, you must re-enable CFStreamCreateBoundPairCompat.
+#endif
+#if defined(__IPHONE_OS_VERSION_MIN_REQUIRED) && (__IPHONE_OS_VERSION_MIN_REQUIRED < 50000)
+#error If you support iOS prior to 5.0, you must re-enable CFStreamCreateBoundPairCompat.
+#endif
+
+    //    if (NO) {
+    //        CFStreamCreateBoundPairCompat(
+    //                                      NULL,
+    //                                      ((inputStreamPtr  != nil) ? &readStream : NULL),
+    //                                      ((outputStreamPtr != nil) ? &writeStream : NULL),
+    //                                      (CFIndex) bufferSize
+    //                                      );
+    //    } else {
+    CFStreamCreateBoundPair(
+                            NULL,
+                            ((inputStreamPtr  != nil) ? &readStream : NULL),
+                            ((outputStreamPtr != nil) ? &writeStream : NULL),
+                            (CFIndex) bufferSize
+                            );
+    //    }
+
+    if (inputStreamPtr != NULL) {
+        *inputStreamPtr  = CFBridgingRelease(readStream);
+    }
+    if (outputStreamPtr != NULL) {
+        *outputStreamPtr = CFBridgingRelease(writeStream);
+    }
+}
+
 
 @end
 

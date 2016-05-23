5d3c16cb1a2035eadc9962989d774881e5727b7f
Justin Henck <jhenck@findyr.com>
2016-05-23 14:50:58 -0400

    Implement TUSFileReader

--- Pod/Classes/TUSFileReader.h
+++ Pod/Classes/TUSFileReader.h
@@ -9,12 +9,25 @@
 #import <Foundation/Foundation.h>
 
 @interface TUSFileReader : NSObject
-- (instancetype)initWithURL:(NSURL *)fileUrl;
-- (NSURL *)getFileFromOffset:(NSUInteger)offset
+-(instancetype)initWithURL:(NSURL *)fileUrl;
+-(NSURL *)getFileFromOffset:(NSUInteger)offset
                        error:(NSError **)error;
 /**
  Clean up the file reader, deleting associated temporary files.  Returns YES if the reader was successfully cleaned up, NO otherwise.
  */
-- (BOOL)close;
+-(BOOL)close;
+
+/**
+ Describe this file reader as a dictionary for saving/restoring
+ */
+-(NSDictionary *)serialize;
+
+/**
+ Restore a file reader from a dictionary
+ */
++(instancetype)deserializeFromDictionary:(NSDictionary *)dictionary;
+
+
+// Properties
 @property (readonly) NSUInteger length;
 @end
--- Pod/Classes/TUSFileReader.m
+++ Pod/Classes/TUSFileReader.m
@@ -10,27 +10,75 @@
 #import "TUSFileReader.h"
 #define COPY_CHUNK_SIZE (10 * 1024 * 1024)
 
+// Serializing keys
+const NSString *TEMP_URL_KEY = @"tempFileUrl";
+const NSString *FILE_URL_KEY = @"fileUrl";
+const NSString *FILE_OFFSET_KEY = @"offset";
+
+// Temporary file subdirectory within Application Support
+const NSString *TEMP_FILE_SUBDIRECTORY = @"TUSKit";
+
+
 @interface TUSFileReader()
 @property (strong, nonatomic) NSURL * fileUrl; // Nonatomic because it won't change once initialized
 @property (strong, nonatomic) NSURL * tempFileUrl;
+@property NSUInteger offset;
 @end
 
 @implementation TUSFileReader{
-    long long _length;
+    long long _length; // Store the computed length of the file
 }
 
 
-- (instancetype)initWithFile:(NSURL*)fileUrl{
+// Designated Initializer
+- (instancetype)initWithURL:(NSURL*)fileUrl{
     self = [super init];
     if (self){
         self.fileUrl = fileUrl;
-        _length = -1;
+        _length = -1; // Set to -1 to force a load of the length from disk.
     }
     return self;
 }
 
+- (instancetype)initWithFileURL:(NSURL*)fileUrl
+                    tempFileURL:(NSURL*)tempFileUrl
+                         offset:(NSUInteger)offset{
+    self = [self initWithURL:fileUrl]; // Call the designated initializer
+    if (self) {
+        self.tempFileUrl = tempFileUrl;
+        self.offset = offset;
+    }
+    return self;
+}
+
+
++(instancetype)deserializeFromDictionary:(NSDictionary *)dictionary{
+    // The URLs were saved as bookmarks, not as paths (because paths might change between launches)
+    NSData *fileUrlData = dictionary[FILE_URL_KEY];
+    NSObject *tempUrlData = dictionary[TEMP_URL_KEY];
+    NSNumber *offset = dictionary[FILE_OFFSET_KEY];
+    
+    NSURL * fileUrl = [NSURL URLByResolvingBookmarkData:fileUrlData options:0 relativeToURL:nil bookmarkDataIsStale:nil error:nil];
+    NSURL * tempUrl = tempUrlData == [NSNull null]? nil : [NSURL URLByResolvingBookmarkData:tempUrlData options:0 relativeToURL:nil bookmarkDataIsStale:nil error:nil];
+    
+    return [[self alloc] initWithFileURL:fileUrl tempFileURL:tempUrl offset:offset.unsignedIntegerValue];
+}
+
 - (NSURL *)getFileFromOffset:(NSUInteger)offset
                        error:(NSError **)error{
+    // Special cases
+    // Offset is zero, so just return the file url itself
+    if (offset == 0){
+        return self.fileUrl;
+    }
+    
+    // Offset is the current offset, so return the current tempFileUrl if a file exists there.
+    if (offset == self.offset && [[NSFileManager defaultManager] fileExistsAtPath:self.tempFileUrl.path]){
+        return self.tempFileUrl;
+    }
+    
+    // Create a new file
+    
     // TODO: How long will this take?
     NSError *fhError = nil;
     NSFileHandle *sourceFile = [NSFileHandle fileHandleForReadingFromURL:self.fileUrl error:&fhError];
@@ -65,6 +113,7 @@
     
     // Synchronize the target file we were writing.
     [targetFile synchronizeFile];
+    self.offset = offset; // Update our offset
     return self.tempFileUrl;
 }
 
@@ -73,7 +122,8 @@
  */
 - (NSURL *)tempFileUrl{
     if (!_tempFileUrl){
-        @throw @"Not implemented";
+        NSURL *applicationSupport = [[NSFileManager defaultManager] URLsForDirectory:NSApplicationSupportDirectory inDomains:NSAllDomainsMask];
+        _tempFileUrl = [[applicationSupport URLByAppendingPathComponent:TEMP_FILE_SUBDIRECTORY isDirectory:YES] URLByAppendingPathComponent:self.fileUrl.filePathURL.lastPathComponent]; // Use filePathURL in case we have a reference URL.
     }
     return _tempFileUrl;
 }
@@ -92,6 +142,16 @@
         }
     }
     return (NSUInteger)_length; // Default to NO length if we can't find the file, the key, etc.
+}
+
+-(NSDictionary *)serialize{
+    // We don't save length because it can be recomputed very easily
+    NSObject * tempFileString = [self.tempFileUrl bookmarkDataWithOptions:NSURLBookmarkCreationSuitableForBookmarkFile includingResourceValuesForKeys:nil relativeToURL:nil error:nil] ?: [NSNull null];
+    return @{
+             TEMP_URL_KEY: tempFileString,
+             FILE_URL_KEY: [self.fileUrl bookmarkDataWithOptions:NSURLBookmarkCreationSuitableForBookmarkFile includingResourceValuesForKeys:nil relativeToURL:nil error:nil    ],
+             FILE_OFFSET_KEY: @(self.offset)
+    };
 }
 
 -(BOOL)close{

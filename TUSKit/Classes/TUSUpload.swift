549d451d73e39c847cee32fb37b2ca530602646b
Mark R. Masterson <mark@masterson.io>
2020-04-05 16:38:21 -0700

    TUSUpload inits

--- TUSKit/Classes/TUSUpload.swift
+++ TUSKit/Classes/TUSUpload.swift
@@ -9,19 +9,27 @@ import UIKit
 
 class TUSUpload: NSObject {
     
+    // MARK: Properties
+    var id: String?
+    var filePath: URL?
+    var data: Data?
+    
     init(withId id: String, andFilePathString filePathString: String) {
-        //
+        self.id = id
+        filePath = URL(string: filePathString)
     }
     
     init(withId id: String, andFilePathURL filePathURL: URL) {
-        //
+        self.id = id
+        filePath = filePathURL
     }
     
     init(withId id: String, andData data: Data) {
-        //
+        self.id = id
+        self.data = data
     }
     
     init(withId id: String) {
-        //
+        self.id = id
     }
 }

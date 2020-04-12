5d0910129aa295ee4eeb76f73abb9df33ca749ba
Mark R. Masterson <mark@masterson.io>
2020-04-12 15:56:32 -0700

    rename

--- Example/TUSKit/AppDelegate.swift
+++ Example/TUSKit/AppDelegate.swift
@@ -17,7 +17,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        TUSClient.shared.setup()
         return true
     }
 
--- Example/TUSKit/ViewController.swift
+++ Example/TUSKit/ViewController.swift
@@ -7,12 +7,14 @@
 //
 
 import UIKit
+import TUSKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        TUSClient.shared()
     }
 
     override func didReceiveMemoryWarning() {

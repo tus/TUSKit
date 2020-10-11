5926ccf0c604377cbadce30d408d7df810c752e3
Mark R. Masterson <mark@masterson.io>
2020-10-10 17:58:53 -0700

    remove dead comments from example

--- Example/TUSKit/ViewController.swift
+++ Example/TUSKit/ViewController.swift
@@ -57,12 +57,9 @@ class ViewController: UIViewController, TUSDelegate, UIImagePickerControllerDele
             let number = Int.random(in: 0 ..< 1000) //TODO: Remove before release: this is only set so we can run multiple files while developer
             
             //When you have a file, create an upload, and give it a Id.
-            //let upload: TUSUpload = TUSUpload(withId: String(format: "%@%@", "img", String(number)), andFilePathURL: file, andFileType: ".jpeg")
             var fileData = try! Data(contentsOf: file)
             let upload: TUSUpload = TUSUpload(withId:  String(number), andData: fileData, andFileType: "jpeg")
-           // TUSUpload(withId: <#T##String#>, andFilePathURL: <#T##URL#>, andFileType: <#T##String#>)
             //Create or resume upload
-           // URL(fileURLWithPath: String)
             TUSClient.shared.createOrResume(forUpload: upload, withCustomHeaders: ["Header": "Value"])
         }
         updateLabel()

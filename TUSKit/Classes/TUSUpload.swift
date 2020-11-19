b02e50a6b2c9c8babb46f7a63af88027fb0569cd
Thomas Bailey <thomas.bailey@exwzd.com>
2020-11-19 17:07:51 +0900

    Refactor TUSUpload id to be non-optional

--- TUSKit/Classes/TUSUpload.swift
+++ TUSKit/Classes/TUSUpload.swift
@@ -24,20 +24,20 @@ public class TUSUpload: NSObject, NSCoding {
     
     public required init?(coder: NSCoder) {
         //
-        id = coder.decodeObject(forKey:"id") as? String
         fileType = coder.decodeObject(forKey:"fileType") as? String
         filePath = coder.decodeObject(forKey:"filePath") as? URL
         uploadLocationURL = coder.decodeObject(forKey:"uploadLocationURL") as? URL
         contentLength = coder.decodeObject(forKey:"contentLength") as? String
         uploadLength = coder.decodeObject(forKey:"uploadLength") as? String
         uploadOffset = coder.decodeObject(forKey:"uploadOffset") as? String
+        id = coder.decodeObject(forKey: "id") as! String
         data = coder.decodeObject(forKey: "data") as? Data
         status = TUSUploadStatus(rawValue: coder.decodeObject(forKey: "status") as! String)
     }
     
     
     // MARK: Properties
-    var id: String?
+    var id: String
     var fileType: String? // TODO: Make sure only ".fileExtension" gets set. Current setup sets fileType as something like "1A1F31FE6-BB39-4A78-AECD-3C9BDE6D129E.jpeg"
     var filePath: URL?
     var data: Data?
@@ -48,27 +48,32 @@ public class TUSUpload: NSObject, NSCoding {
     var status: TUSUploadStatus?
     
     public init(withId id: String, andFilePathString filePathString: String, andFileType fileType: String) {
-        super.init()
         self.id = id
         filePath = URL(string: filePathString)
         self.fileType = fileType
 
+        super.init()
     }
     
     public init(withId id: String, andFilePathURL filePathURL: URL, andFileType fileType: String) {
-        super.init()
         self.id = id
         filePath = filePathURL
         self.fileType = fileType
+
+        super.init()
     }
     
     public init(withId id: String, andData data: Data, andFileType fileType: String) {
         self.id = id
         self.data = data
         self.fileType = fileType
+        
+        super.init()
     }
     
     public init(withId id: String) {
         self.id = id
+        
+        super.init()
     }
 }

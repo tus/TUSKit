9d1ecc9cc6a7428bae017e2b513b6d3baa072574
Tjeerd <tjeerd@twinapps.co>
2021-09-15 14:35:19 +0200

    Cleaning up TUSClient a bit in regards to loading files by URL

--- Sources/TUSKit/TUSClient.swift
+++ Sources/TUSKit/TUSClient.swift
@@ -13,7 +13,8 @@ public struct TUSClientError: Error {
 
     // Maintenance: We use static lets on a struct, instead of an enum, so that adding new cases won't break stability.
     // Alternatively we can ask users to always use `unknown default`, but we can't guarantee that everyone will use that.
-    public static let fileNotFound = TUSClientError(code: 1)
+    public static let couldNotCopyFile = TUSClientError(code: 1)
+    public static let filesizeNotKnown = TUSClientError(code: 2)
 }
 
 /// The TUSKit client.
@@ -42,24 +43,17 @@ public final class TUSClient {
     /// - Throws: TUSClientError
     public func uploadFileAt(filePath: URL) throws {
         do {
-            let url = try Files.copy(from: filePath)
-            guard let size = try url.resourceValues(forKeys: [.fileSizeKey]).fileSize else {
-                print("Size is unknown")
-                // TODO: Load data and try getting size
-                throw TUSClientError.fileNotFound
-            }
+            let destinationFilePath = try Files.copy(from: filePath)
+            let size = try destinationFilePath.resourceValues(forKeys: [.fileSizeKey]).fileSize ?? Data(contentsOf: destinationFilePath).count
             
-            // TODO: Duplicate
+            // TODO: Duplicate size
             let sizeInKiloBytes = 500 * 1024// Uses safe speed, 500kb
             let ranges = (0..<size).chunkRanges(size: sizeInKiloBytes)
             
-            scheduleUploadsFor(url: url, ranges: ranges)
+            scheduleUploadsFor(filePath: destinationFilePath, ranges: ranges)
         } catch {
-            throw TUSClientError.fileNotFound
+            throw TUSClientError.couldNotCopyFile
         }
-//        // TODO: Copy file from URL
-//        let data = try findData(for: filePath)
-//        scheduleUploadsFor(data: data)
     }
     
     /// Upload data
@@ -90,35 +84,26 @@ public final class TUSClient {
         // TODO: error handling
         
         // Store file now
-        let url = try! Files.store(data: data)
+        // TODO: Force
+        let filePath = try! Files.store(data: data)
         let sizeInKiloBytes = 500 * 1024// Uses safe speed, 500kb
         let ranges = data.chunkRanges(size: sizeInKiloBytes)
-        scheduleUploadsFor(url: url, ranges: ranges)
+        scheduleUploadsFor(filePath: filePath, ranges: ranges)
     }
     
-    private func scheduleUploadsFor(url: URL, ranges: [Range<Int>]) {
+    /// Start upload based on a file path
+    /// - Parameters:
+    ///   - filePath: The url to a file's location
+    ///   - ranges: The ranges of the file to upload
+    private func scheduleUploadsFor(filePath: URL, ranges: [Range<Int>]) {
         let tasks = ranges.map { range in
-            UploadDataTask(url: url, range: range, uploader: Uploader())
+            UploadDataTask(filePath: filePath, range: range, uploader: Uploader())
         }
         
         total += tasks.count
         scheduler.addGroupedTasks(tasks: tasks)
     }
 
-    /// Get Data based on URL
-    /// - Parameter url: The target url to load data from
-    /// - Throws: Throws TUSClientError
-    /// - Returns: The loaded Data
-    private func findData(for url: URL) throws -> Data {
-        do {
-            return try Data(contentsOf: url)
-        } catch {
-            let prefixedPath = "file://" + url.absoluteString
-            let url = try URL(string:prefixedPath).or(willThrow: TUSClientError.fileNotFound)
-            let data = try? Data(contentsOf: url)
-            return try data.or(willThrow: TUSClientError.fileNotFound)
-        }
-    }
 }
 
 extension TUSClient: SchedulerDelegate {
@@ -140,20 +125,19 @@ extension TUSClient: SchedulerDelegate {
 
 final class UploadDataTask: Task {
     
-//    let chunk: Data
-    let url: URL
+    let filePath: URL
     let range: Range<Int>
     let uploader: Uploader
     
-    init(url: URL, range: Range<Int>, uploader: Uploader) {
-        self.url = url
+    init(filePath: URL, range: Range<Int>, uploader: Uploader) {
+        self.filePath = filePath
         self.range = range
         self.uploader = uploader
     }
     
     func run(completed: @escaping ([Task]) -> ()) {
         // TODO: Error handling
-        let data = try! Data(contentsOf: url)
+        let data = try! Data(contentsOf: filePath)
         let sub = data[range]
         
         uploader.upload(data: sub, range: range) {

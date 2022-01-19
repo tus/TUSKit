c325d353abd9cbe7bebeca482b103237ab9eed68
Elvirion Antersijn <elvirion@silvercode.nl>
2022-01-19 08:16:07 +0100

    Made public Dictionary extension function internal

--- Tests/TUSKitTests/Support/Support.swift
+++ Tests/TUSKitTests/Support/Support.swift
@@ -66,7 +66,7 @@ extension Dictionary {
     
     /// Case insenstiive subscripting. Only for string keys.
     /// We downcast to string to support AnyHashable keys.
-    public subscript(caseInsensitive key: Key) -> Value? {
+    subscript(caseInsensitive key: Key) -> Value? {
         guard let someKey = key as? String else {
             return nil
         }

7363264c04a01a239238f6409137ffb692ca63d3
Damir Tursunović <hello@damir.me>
2022-09-12 22:30:48 +0200

    Fix compile error Xcode 14
    Lazy properties are now considered stored properties, so they can no
    longer be marked unavailable. This commit works around this by using a
    computed var with backed storage.

--- Sources/TUSKit/TUSClient.swift
+++ Sources/TUSKit/TUSClient.swift
@@ -76,10 +76,18 @@ public final class TUSClient {
     private var uploads = [UUID: UploadMetadata]()
     
 #if os(iOS)
+    private var _backgroundClient: Any?
+    
     @available(iOS 13.0, *)
-    private lazy var backgroundClient: TUSBackground = {
-        return TUSBackground(api: api, files: files, chunkSize: chunkSize)
-    }()
+    /// Lazy properties are considered as stored properties in Swift 5.7, so they can no longer be marked as unavailable. Hence
+    /// the computed property backed by storage var.
+    private var backgroundClient: TUSBackground? {
+        if _backgroundClient == nil {
+            _backgroundClient = TUSBackground(api: api, files: files, chunkSize: chunkSize)
+        }
+        
+        return _backgroundClient as? TUSBackground
+    }
 #endif
     
     /// Initialize a TUSClient
@@ -289,7 +297,7 @@ public final class TUSClient {
 #if os(iOS)
     @available(iOS 13.0, *)
     public func scheduleBackgroundTasks() {
-        backgroundClient.scheduleBackgroundTasks()
+        backgroundClient?.scheduleBackgroundTasks()
     }
 #endif
     

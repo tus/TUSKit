96b08521a2c12531fc5a774c2fb26dfa6ce32b57
Sai Raghu Varma Kallepalli <srvarma7@gmail.com>
2023-07-11 18:59:18 +0530

    HTTPURLResponse clean up

--- Sources/TUSKit/Extensions/HTTPURLResponse+Headers.swift
+++ Sources/TUSKit/Extensions/HTTPURLResponse+Headers.swift
@@ -1,5 +1,5 @@
 //
-//  File.swift
+//  HTTPURLResponse+Headers.swift
 //  
 //
 //  Created by Sai Raghu Varma Kallepalli on 11/07/23.
@@ -10,10 +10,8 @@ import Foundation
 extension HTTPURLResponse {
     func extractHeaders() -> [String: String] {
         var headers: [String: String] = [:]
-        for header in allHeaderFields {
-            let key1 = String(describing: header.key)
-            let value1 = String(describing: header.value)
-            headers[key1] = value1
+        allHeaderFields.forEach { key, value in
+            headers[String(describing: key)] = String(describing: value)
         }
         return headers
     }

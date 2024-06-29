c3b76f2553a19242e55ddc3665b38ff34832474d
Donny Wals <donnywals@gmail.com>
2024-06-29 14:46:26 +0100

    Updated networking extensions to work with concurrency

--- Sources/TUSKit/Network.swift
+++ Sources/TUSKit/Network.swift
@@ -11,20 +11,42 @@ enum NetworkError: Error {
     case noHTTPURLResponse
 }
 
-// Result support for URLSession
+// URLSession conveniences
 extension URLSession {
+    func data(request: URLRequest) async throws -> (Data, HTTPURLResponse) {
+        return try await validate(data(for: request))
+    }
     
+    func upload(request: URLRequest, data: Data) async throws -> (Data, HTTPURLResponse) {
+        return try await validate(upload(for: request, from: data))
+    }
+    
+    func upload(request: URLRequest, fromFile file: URL) async throws -> (Data, HTTPURLResponse) {
+        return try await validate(upload(for: request, fromFile: file))
+    }
+    
+    @available(*, deprecated, message: "use async data(request:) instead")
     func dataTask(request: URLRequest, completion: @escaping (Result<(Data?, HTTPURLResponse), Error>) -> Void) -> URLSessionDataTask {
         return dataTask(with: request, completionHandler: makeCompletion(completion: completion))
     }
     
+    @available(*, deprecated, message: "use async upload(request:data:) instead")
     func uploadTask(request: URLRequest, data: Data, completion: @escaping (Result<(Data?, HTTPURLResponse), Error>) -> Void) -> URLSessionUploadTask {
         return uploadTask(with: request, from: data, completionHandler: makeCompletion(completion: completion))
     }
     
+    @available(*, deprecated, message: "use async upload(request:fromFile:) instead")
     func uploadTask(with request: URLRequest, fromFile file: URL, completion: @escaping (Result<(Data?, HTTPURLResponse), Error>) -> Void) -> URLSessionUploadTask {
         return uploadTask(with: request, fromFile: file, completionHandler: makeCompletion(completion: completion))
     }
+}
+
+private func validate(_ tuple: (Data, URLResponse)) throws -> (Data, HTTPURLResponse) {
+    guard let response = tuple.1 as? HTTPURLResponse else {
+        throw NetworkError.noHTTPURLResponse
+    }
+    
+    return (tuple.0, response)
 }
 
 /// Convenience method to turn a URLSession completion handler into a modern Result version. It also checks if response is a HTTPURLResponse

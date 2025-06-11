1b1073df4316a346b006446ee23ac1901744a670
Donny Wals <donnywals@gmail.com>
2025-06-11 15:52:05 +0200

    Fix annotation for streamingData

--- Sources/TUSKit/Files.swift
+++ Sources/TUSKit/Files.swift
@@ -157,7 +157,7 @@ final class Files {
         }
     }
     
-    @available(iOS 13.4, *)
+    @available(iOS 13.4, macOS 10.15, *)
     func streamingData(_ dataGenerator: () -> Data?, id: UUID, preferredFileExtension: String? = nil) throws -> URL {
         try queue.sync {
             try makeDirectoryIfNeeded()
